76b3fcd2bf700566d7c6afd87266977099221bdd
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-10-05 20:22:10 +0900

    ✨ Add Auth Page Routes

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,7 +1,11 @@
 import Main from '../components/Main/index'
 import Mypage from '../components/Mypage/index'
+import Signin from '../components/Auth/Signin/index'
+import Signup from '../components/Auth/Signup/index'
 
 export default [
     { path: '*', component: <Main/> },
-    { path: '/mypage', component: <Mypage/> }
+    { path: '/mypage', component: <Mypage/> },
+    { path: '/signin', component: <Signin/> },
+    { path: '/signup', component: <Signup/> },
 ]

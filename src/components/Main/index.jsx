edc6c510d92a501de360dc4cd53db919e3323b15
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-10-05 22:15:21 +0900

    ✔️ Main Slide Text, Lobby Button Update

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,21 +3,38 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import sliderImage1 from '../../images/Main/sliderImage1.png'
-import sliderImage2 from '../../images/Main/sliderImage1.png'
-import sliderImage3 from '../../images/Main/sliderImage1.png'
+import sliderImage2 from '../../images/Main/sliderImage2.png'
+import sliderImage3 from '../../images/Main/sliderImage3.png'
 
 const Render = () => {
+    const sliderImageList = [sliderImage1, sliderImage2, sliderImage3]
     const settings = {
         arrows: false,
         dots: true,
         infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000  
     };
     return (
         <style.background>
             <style.slider {...settings}>
-                <style.sliderImage bg={sliderImage1}/>
-                <style.sliderImage bg={sliderImage1}/>
+                {
+                    sliderImageList.map((element, idx) => {
+                        return (
+                            <style.sliderImage bg={element} key={idx}>
+                                <p>
+                                <style.sliderText>여행을 떠나고 싶은데,</style.sliderText>
+                                </p>
+                                <style.sliderText color='#00ffc2'>같이 떠날 사람</style.sliderText>
+                                <style.sliderText>이 없다면?</style.sliderText>
+                                <style.sliderUnderText>바로가기 →</style.sliderUnderText>
+                            </style.sliderImage>
+                        )
+                    })
+                }
             </style.slider>
+            <style.navigateButton>같이 떠날 사람 찾아보기</style.navigateButton>
         </style.background>
     )
 }
--- src/components/Main/index.style.js
+++ src/components/Main/index.style.js
@@ -5,7 +5,9 @@ export const background = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    gap: 10%;
     position: relative;
     background-color: white;
 `
@@ -14,11 +16,44 @@ export const slider = styled(Slider)`
     height: 550px;
 `
 export const sliderImage = styled.div`
+    padding-top: 7%;
+    padding-left: 5%;
     width: 100%;
     height: 550px;
+    position: relative;
     background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url(${(props) => props.bg});
     background-size: cover;
 `
-export const sliderArrow = styled.div`
-    background-color: lightgray;
+export const sliderText = styled.span`
+    font-size: 64px;
+    font-weight: bold;
+    color: ${(props) => props.color ? props.color : 'white'};
+`
+export const sliderUnderText = styled(sliderText)`
+    position: absolute;
+    font-size: 48px;
+    right: 5%;
+    bottom: 5%;
+`
+export const navigateButton = styled.button`
+    width: 600px;
+    height: 75px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 34px;
+    color: white;
+    background-color: #0066FF;
+    border: none;
+    border-radius: 50px;
+    font-weight: bold;
+    transition: .15s ease-in-out;
+    cursor: pointer;
+    &:hover {
+        background-color: #0044FF;
+    }
+    > span {
+        color: white;
+        font-weight: bold;
+    }
 `

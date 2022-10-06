68fa0a533bfd50eb00b6e7aa3e0c06cbc0dd2b92
KJS <digyrh456789@gmail.com>
2022-10-06 10:53:12 +0900

    Create Signup Page(Image loader)

--- src/components/Auth/signup/third/index.jsx
+++ src/components/Auth/signup/third/index.jsx
@@ -1,10 +1,27 @@
 import * as style from './index.style';
 import { useNavigate } from 'react-router-dom';
+import uploadbutton from '../../../../images/Main/uploade.png'
+import img from '../../../../images/Main/Group 65.png'
+import { useState, useRef } from 'react';
 const Render = () => {
     const nav = useNavigate();
     const fourthsign = () => {
         nav('/signup4')
     };
+    const encodeFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        return new Promise((resolve) => {
+            reader.onload = () => {
+                setImageSrc(reader.result);
+                resolve();
+            };
+        });
+    }
+    const [imageSrc, setImageSrc] = useState('https://cdn-icons-png.flaticon.com/512/1250/1250689.png');
+    const handleChangeFile = (e) => {
+        encodeFile(e.target.files[0]);
+    }
     return (
         <style.background>
             <style.loginbox>
@@ -13,6 +30,18 @@ const Render = () => {
                     <p>당신이 가고 싶은 곳을 원하는 사람을 골라 함께 떠나보세요</p>
                 </style.whemanchu>
                 <h1>Welcome to 외만추</h1>
+                <h2>3/4</h2>
+                <p className='profile'>프로필 사진</p>
+                <div class="filebox">
+                    <div className='pro'>
+                        {imageSrc && <img src={imageSrc} alt="preview-img" />}
+                    </div>
+                    <label for="file">파일 선택</label>
+                    <input type="file" id="file" onChange={handleChangeFile} multiple="multiple" />
+                </div>
+                <p className='ment'>자기소개</p>
+                <input type="text" className='textareas' placeholder='자기소개를 입력해주세요(20자~500자)' />
+                <button className='nextb' onClick={fourthsign}><text>Next →</text></button>
             </style.loginbox>
         </style.background>
     );
--- src/components/Auth/signup/third/index.style.js
+++ src/components/Auth/signup/third/index.style.js
@@ -20,6 +20,80 @@ export const loginbox = styled.div`
         margin-left: 47vw;
         margin-top: 42px;
     }
+    > h2 {
+        font-size: 30px;
+        margin-lefT: 54vw;
+        margin-top: -2vh;
+    }
+    > .profile {
+        margin-left: 45.4vw;
+        margin-top: 1vh;
+        color: #AFAFAF;
+    }
+    
+    > .filebox {
+        position:absolute;
+        margin-left: 5vw;
+        margin-top: 5vh;
+        > .pro {
+            margin-left: 40.3vw;
+            margin-top: -5vh;
+            > img {
+            width: 8vw;
+            height: 16vh;
+            }
+        }
+        > label {
+            display: inline-block;
+            margin-top: -20vh;
+            padding: 10px 20px;
+            color: #fff;
+            vertical-align: middle;
+            background-color: #597DFF;
+            cursor: pointer;
+            height: 40px;
+            margin-left: 50vw;
+            border-radius: 5px;
+        }
+        > input[type="file"] {
+            position: absolute;
+            width: 0;
+            height: 0;
+            padding: 0;
+            overflow: hidden;
+            border: 0;
+        }
+    }
+    > .ment {
+        position: absolute;
+        margin-left: 45.4vw;
+        margin-top: 18vh;
+        color: #AFAFAF;
+    }
+    .textareas {
+        margin-left: 45.4vw;
+        margin-top: 23vh;
+        width: 20vw;
+        height: 15vh;
+        padding-left: 1vw;
+    }
+    .nextb {
+        margin-top: 6vh;
+        margin-left: 46vw;
+        width: 20vw;
+        height: 7vh;
+        border: 0px;
+        border-radius: 10px;
+        background-color: #597DFF;
+        > text{
+            margin: auto;
+            color: white;
+            font-size: 20px;
+        }
+        :hover {
+            cursor: pointer;
+        }
+    }
 `
 
 export const whemanchu = styled.div`
